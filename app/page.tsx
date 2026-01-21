4825805e8245fe6d6eb41383fc68366b4a80dd28
Mario Morera <mariomorera@Mac-mini-de-Mario.local>
2026-01-21 00:22:21 -0300

    chore: remove ROI calculator section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { HeroSection } from '@/components/sections/HeroSection';
 import { ProblemSolution } from '@/components/sections/ProblemSolution';
 import { ServicesCatalog } from '@/components/sections/ServicesCatalog';
 import { Automations } from '@/components/sections/Automations';
-import { ROICalculator } from '@/components/sections/ROICalculator';
+
 import { PricingTable } from '@/components/sections/PricingTable';
 import { Portfolio } from '@/components/sections/Portfolio';
 import { ContactForm } from '@/components/sections/ContactForm';
@@ -22,7 +22,7 @@ export default function Home() {
 
       <Automations />
 
-      <ROICalculator />
+
 
       <PricingTable />
 
